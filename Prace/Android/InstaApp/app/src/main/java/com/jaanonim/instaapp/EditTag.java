d464bdffac60d965d94bb11f442506e6dd1603ba
jaanonim-school <randomtestmail2@gmail.com>
2022-06-02 11:45:37 +0200

    Android in work

---
+++ Prace/Android/InstaApp/app/src/main/java/com/jaanonim/instaapp/EditTag.java
@@ -0,0 +1,14 @@
+package com.jaanonim.instaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditTag extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_tag);
+    }
+}
