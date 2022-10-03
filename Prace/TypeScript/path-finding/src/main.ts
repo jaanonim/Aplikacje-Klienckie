aa4ecce69e1f3d12f23529912dcee28c8945c494
Jaanonim <mat8mro@gmail.com>
2022-10-03 12:28:06 +0200

    path finding small fix

--- Prace/TypeScript/path-finding/src/main.ts
+++ Prace/TypeScript/path-finding/src/main.ts
@@ -1,21 +1,8 @@
-import Color from "./math/Color";
 import Map from "./Map";
-
 import "./style.css";
-
-const map = [
-    [0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 1, 1, 0, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0],
-    [0, 1, 1, 1, 0, 1, 1, 0],
-    [0, 0, 1, 0, 0, 0, 1, 1],
-    [0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 1, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0],
-];
 
 const ele = document.getElementById("root");
 const ele2 = document.getElementById("list");
 if (ele && ele2) {
-    const m = new Map(8, 8, ele, ele2);
+    new Map(8, 8, ele, ele2);
 }
